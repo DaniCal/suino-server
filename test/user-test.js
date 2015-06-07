17f4cc961dc9b2fcdb759023d559fad2fd1599d2
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-06-07 13:29:02 +0200

    basic test framework structure added (mocha, should, supertest)

---
+++ test/user-test.js
@@ -0,0 +1,13 @@
+var should = require('should');
+var supertest = require('supertest');
+
+describe ('Users', function (){
+
+    it('should pass', function(done){
+        done();
+    });
+
+    it('should not pass', function (done){
+        done();
+    });
+});
