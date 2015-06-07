7dc07ce89b8b484c1b021f9408edc939cdd3ce2f
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-06-07 13:33:14 +0200

    basic user model and controller structure added

---
+++ app/controllers/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+exports.load = function (req, res, next, id) {
+
+};
+
+
+exports.create = function (req, res) {
+
+};
---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+/**
+ * User Schema
+ */
+
+var UserSchema = new Schema({
+    name: { type: String, default: '' },
+    email: { type: String, default: '' },
+    username: { type: String, default: '' }
+});
+
+
+/**
+ * Methods
+ */
+
+UserSchema.methods = {
+
+}
+
+
+
+
+mongoose.model('User', UserSchema);
