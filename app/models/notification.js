23142a53ccdcf66bd68507a834b71a85a0b76bd1
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-11-02 16:11:35 +0100

    notfication model created - basic function heads

---
+++ app/models/notification.js
@@ -0,0 +1,69 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var NotificationSchema = new Schema({
+    userId: {type: String},
+    type: {type: String},
+    message: {type: String},
+    token: {type: String},
+    read: {type: Boolean}
+});
+
+var notificationType = {
+    reservation: 1,
+    canceling: 2,
+    feedbackRequest: 3,
+    feedbackReceived: 4,
+    publicMessage: 5
+};
+
+var NotificationModel = mongoose.model('Notification', NotificationSchema);
+
+var Notification = function(){};
+
+Notification.createReservationNotification = function(data, callback){
+
+};
+
+Notification.createCancelingNotification = function(data, callback){
+
+};
+
+Notification.createFeedbackRequestNotification = function(data, callback){
+
+};
+
+Notification.createFeedbackReceivedNotification = function(data, callback){
+
+};
+
+Notification.createPublicMessageNotification = function (data, callback){
+
+};
+
+Notification.readNotification = function (data, callback){
+
+};
+
+Notification.query = function(data, callback){
+
+};
+
+
+var createNotificationMessage = function(){
+
+};
+
+var isNotificationDataValid = function(data){
+    if(data == null || data == undefined){
+        return false;
+    }else if(data.userId == undefined || data.type == undefined
+    || data.token == undefined ){
+        return false;
+    }else{
+        return true;
+    }
+};
+
+
+
